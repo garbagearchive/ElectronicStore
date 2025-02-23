ef01d352b78a1bb055f40498260f44c74ef564ca
T T <hdang4747@gmail.com>
2025-02-23 22:47:58 +0700

    Testing API

---
+++ FinalAPIDoAn/Controllers/Anothercs.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalAPIDoAn.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Anothercs : ControllerBase
+    {
+    }
+}
