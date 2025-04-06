206cc7a5fd2d653eb37154b8cc7dbdd20715ac1d
Mon2005 <146831103+Mon2005@users.noreply.github.com>
2025-04-06 12:34:32 +0700

    Delete LoginResponseDto.cs

--- LoginResponseDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FinalAPIDoAn.Models.DTOs
-{
-    public class LoginResponseDto
-    {
-        public UserDto User { get; set; } = null!;
-        public string Token { get; set; } = null!; // Token JWT
-    }
-}
