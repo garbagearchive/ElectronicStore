8729fd946cc23296060a4edacb46cf7b48071a05
T T <hdang4747@gmail.com>
2025-04-06 17:53:43 +0700

    revert

---
+++ FinalAPIDoAn/Controllers/AdminEndpoint.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalAPIDoAn.Controllers
+{
+    [Route("api/admin")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        // Chỉ người dùng có vai trò Admin mới có thể truy cập endpoint này.
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public IActionResult GetAdminData()
+        {
+            // Xử lý logic ở đây.
+            return Ok(new { message = "Dữ liệu chỉ dành cho Admin." });
+        }
+    }
+}
